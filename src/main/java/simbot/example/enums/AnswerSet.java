8efea8b4e637294491b4d2cbc9b4851f448b98ba
chenjie <18712443531@163.com>
2022-10-19 09:16:22 +0800

    增加对话枚举类

---
+++ src/main/java/simbot/example/enums/AnswerSet.java
@@ -0,0 +1,18 @@
+package simbot.example.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum AnswerSet {
+
+
+
+    ;
+
+
+    private final String reply;
+
+    AnswerSet(String reply) {
+        this.reply = reply;
+    }
+}
