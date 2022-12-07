a61d85d18b8136227ac66882f1863f4056fbbb0f
chenjie <18712443531@163.com>
2022-12-07 15:09:26 +0800

    配置调整

--- src/main/java/simbot/cycle/SpringBootWebJavaApplication.java
+++ src/main/java/simbot/cycle/SpringBootWebJavaApplication.java
@@ -1,14 +1,9 @@
 package simbot.cycle;
 
-import love.forte.simboot.autoconfigure.EnableSimbot;
+import love.forte.simboot.spring.autoconfigure.EnableSimbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Spring的启动类。当你需要使用Simbot的时候，需要标记 {@link EnableSimbot}
- *
- * @author forte
- */
 @EnableSimbot
 @SpringBootApplication
 public class SpringBootWebJavaApplication {
